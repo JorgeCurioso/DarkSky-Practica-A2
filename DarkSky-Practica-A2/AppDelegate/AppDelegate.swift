5fdb8a033eed051599bd728d35b00389d4918d2f
George Royce <george.royce@detroitlabs.com>
2018-04-15 15:23:42 -0400

    Initial commit

---
+++ DarkSky-Practica-A2/AppDelegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  DarkSky-Practica-A2
+//
+//  Created by George Royce on 4/15/18.
+//  Copyright © 2018 George Royce. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
