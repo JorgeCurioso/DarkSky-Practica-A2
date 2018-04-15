e642bea54f40fa278e35e89102fe70519521ed1f
George Royce <george.royce@detroitlabs.com>
2018-04-15 16:12:15 -0400

    Parse response into model for current weather

---
+++ DarkSky-Practica-A2/Models/DailyWeatherForecast.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWeatherForecast.swift
+//  DarkSky-Practica-A2
+//
+//  Created by George Royce on 4/15/18.
+//  Copyright © 2018 George Royce. All rights reserved.
+//
+
+import Foundation
+
+struct DailyWeatherForecast {
+    
+}
+
+struct RawDailyWeatherForecast: Codable {
+    
+}
--- DarkSky-Practica-A2/Services/WeatherService.swift
+++ DarkSky-Practica-A2/Services/WeatherService.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 
+private struct WeatherInfo: Codable {
+    
+    let currently: RawCurrentWeatherForecast
+    let daily: RawDailyWeatherForecast
+    
+}
+
 class WeatherSerivce {
     //gather credentials
 //    https://api.darksky.net/forecast/0123456789abcdef9876543210fedcba/42.3601,-71.0589
@@ -15,9 +22,7 @@ class WeatherSerivce {
     private let longitude = -71.0589
     private let darkSkyScheme = "https"
     private let darkSkyHost = "api.darksky.net"
-    private let darkSkyKey = ""
     
-    //compose url
     private func darkSkyURL() -> URL? {
         var components = URLComponents()
         components.scheme = darkSkyScheme
@@ -31,7 +36,6 @@ class WeatherSerivce {
         return "/forecast/\(darkSkyKey)/\(latitude),\(longitude)"
     }
     
-    //pass url in to NetworkManager to fetch data
     func fetchCurrentWeatherForecast(completion: () -> ()) {
         guard let url = darkSkyURL() else {
             print("could not construct url")
@@ -40,7 +44,14 @@ class WeatherSerivce {
         
         NetworkManager.getDataFor(url: url) { (data) in
             //take data and parse into models
-            
+            do {
+                let weatherInfo = try JSONDecoder().decode(WeatherInfo.self, from: data)
+                let currentForecast = CurrentWeatherForecast(weatherInfo.currently)
+                print("currentForecast: \(currentForecast.temperature)")
+            }
+            catch {
+                print("could not parse data into models")
+            }
         }
     }
 }
